faf180e95654af29324caa0fc0fac869f200202b
Alessandro Bellina <abellina@nvidia.com>
2020-12-13 11:39:48 -0600

    testing

--- src/ucs/profile/profile_on.h
+++ src/ucs/profile/profile_on.h
@@ -116,9 +116,11 @@ BEGIN_C_DECLS
     static UCS_F_ALWAYS_INLINE _ret_type _name##_inner(__VA_ARGS__); \
     \
     _ret_type _name(__VA_ARGS__) { \
-        _ret_type _ret = _name##_inner _arglist; \
+        nvtxRangePush(_name); \
         UCS_PROFILE_SCOPE_BEGIN(); \
+        _ret_type _ret = _name##_inner _arglist; \
         UCS_PROFILE_SCOPE_END(#_name); \
+        nvtxRangePop(); \
         return _ret; \
     } \
     static UCS_F_ALWAYS_INLINE _ret_type _name##_inner(__VA_ARGS__)
